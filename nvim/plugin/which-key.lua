6919f1b933f0ee023ca8454fdfc9d2f9d24c7df3
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-12 01:37:09 -0500

    feat(wk): update to lazy-load

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -1,3 +1,12 @@
-require('which-key').setup {
-  preset = 'helix'
-}
+require("lze").load({
+  "which-key.nvim",
+  event = "VimEnter",
+  keys = {
+    {
+      "<leader>?",
+      function() require("which-key").show({ global = false }) end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+  after = function(_) require("which-key").setup({ icons = { mappings = false } }) end,
+})
