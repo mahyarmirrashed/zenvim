10b4953756d31104d599ffc4db3fd409ebc9700c
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-12 18:10:06 -0500

    style(lua): format module index

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,7 +11,7 @@ local opt = vim.o
 -- See :h <option> to see what the options do
 
 -- Search down into subfolders
-opt.path = vim.o.path .. '**'
+opt.path = vim.o.path .. "**"
 
 opt.number = true
 opt.relativenumber = true
@@ -22,7 +22,7 @@ opt.incsearch = true
 opt.hlsearch = true
 
 opt.spell = true
-opt.spelllang = 'en'
+opt.spelllang = "en"
 
 opt.expandtab = true
 opt.tabstop = 2
@@ -30,48 +30,38 @@ opt.softtabstop = 2
 opt.shiftwidth = 2
 opt.foldenable = true
 opt.history = 2000
-opt.nrformats = 'bin,hex' -- 'octal'
+opt.nrformats = "bin,hex" -- 'octal'
 opt.undofile = true
 opt.splitright = true
 opt.splitbelow = true
 opt.cmdheight = 0
 
 opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-opt.colorcolumn = '100'
+opt.colorcolumn = "100"
 
 -- Configure Neovim diagnostic messages
 
-local function prefix_diagnostic(prefix, diagnostic)
-  return string.format(prefix .. ' %s', diagnostic.message)
-end
+local function prefix_diagnostic(prefix, diagnostic) return string.format(prefix .. " %s", diagnostic.message) end
 
-vim.diagnostic.config {
+vim.diagnostic.config({
   virtual_text = {
-    prefix = '',
+    prefix = "",
     format = function(diagnostic)
       local severity = diagnostic.severity
-      if severity == vim.diagnostic.severity.ERROR then
-        return prefix_diagnostic('󰅚', diagnostic)
-      end
-      if severity == vim.diagnostic.severity.WARN then
-        return prefix_diagnostic('⚠', diagnostic)
-      end
-      if severity == vim.diagnostic.severity.INFO then
-        return prefix_diagnostic('ⓘ', diagnostic)
-      end
-      if severity == vim.diagnostic.severity.HINT then
-        return prefix_diagnostic('󰌶', diagnostic)
-      end
-      return prefix_diagnostic('■', diagnostic)
+      if severity == vim.diagnostic.severity.ERROR then return prefix_diagnostic("󰅚", diagnostic) end
+      if severity == vim.diagnostic.severity.WARN then return prefix_diagnostic("⚠", diagnostic) end
+      if severity == vim.diagnostic.severity.INFO then return prefix_diagnostic("ⓘ", diagnostic) end
+      if severity == vim.diagnostic.severity.HINT then return prefix_diagnostic("󰌶", diagnostic) end
+      return prefix_diagnostic("■", diagnostic)
     end,
   },
   signs = {
     text = {
       -- Requires Nerd fonts
-      [vim.diagnostic.severity.ERROR] = '󰅚',
-      [vim.diagnostic.severity.WARN] = '⚠',
-      [vim.diagnostic.severity.INFO] = 'ⓘ',
-      [vim.diagnostic.severity.HINT] = '󰌶',
+      [vim.diagnostic.severity.ERROR] = "󰅚",
+      [vim.diagnostic.severity.WARN] = "⚠",
+      [vim.diagnostic.severity.INFO] = "ⓘ",
+      [vim.diagnostic.severity.HINT] = "󰌶",
     },
   },
   update_in_insert = false,
@@ -79,17 +69,17 @@ vim.diagnostic.config {
   severity_sort = true,
   float = {
     focusable = false,
-    style = 'minimal',
-    border = 'rounded',
-    source = 'if_many',
-    header = '',
-    prefix = '',
+    style = "minimal",
+    border = "rounded",
+    source = "if_many",
+    header = "",
+    prefix = "",
   },
-}
+})
 
 -- Native plugins
-cmd.filetype('plugin', 'indent', 'on')
-cmd.packadd('cfilter') -- Allows filtering the quickfix list with :cfdo
+cmd.filetype("plugin", "indent", "on")
+cmd.packadd("cfilter") -- Allows filtering the quickfix list with :cfdo
 
 -- let sqlite.lua (which some plugins depend on) know where to find sqlite
-vim.g.sqlite_clib_path = require('luv').os_getenv('LIBSQLITE')
+vim.g.sqlite_clib_path = require("luv").os_getenv("LIBSQLITE")
