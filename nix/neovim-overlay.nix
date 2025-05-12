6e7bc09666770f2e935496c58250f5cb1067dc3a
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-12 18:56:58 -0500

    feat(comment): add easier method to comment in/out lines

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -52,6 +52,7 @@ let
     # >>> Miscellaneous
     better-escape-nvim # https://github.com/max397574/better-escape.nvim
     cloak-nvim # https://github.com/laytan/cloak.nvim
+    comment-nvim # https://github.com/numToStr/Comment.nvim
     nvim-autopairs # https://github.com/windwp/nvim-autopairs
     which-key-nvim # https://github.com/folke/which-key.nvim
     # >>> Plugin dependencies
