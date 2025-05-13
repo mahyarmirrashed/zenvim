4d813f5f4e28589c2d8c54ad503a402d9fd555d7
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-12 22:43:11 -0500

    style(lua): format code

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -7,29 +7,29 @@ local spelldir = vim.fn.stdpath("data") .. "/spell"
 vim.fn.mkdir(spelldir, "p")
 
 -- General
-vim.opt.clipboard = "unnamedplus"         -- use system clipboard (`+` register)
-vim.opt.mouse = "a"                       -- enable mouse support in all modes
-vim.opt.spell = true                      -- enable spell checking
-vim.opt.spellcapcheck = ""                -- disable capitalization check
+vim.opt.clipboard = "unnamedplus" -- use system clipboard (`+` register)
+vim.opt.mouse = "a" -- enable mouse support in all modes
+vim.opt.spell = true -- enable spell checking
+vim.opt.spellcapcheck = "" -- disable capitalization check
 vim.opt.spellfile = spelldir .. "/en.add" -- specify spell file directory
-vim.opt.spelllang = "en"                  -- specify language for spell checking
-vim.opt.swapfile = false                  -- disable swap files to avoid clutter
-vim.opt.undofile = true                   -- enable undo files for persistent undo
+vim.opt.spelllang = "en" -- specify language for spell checking
+vim.opt.swapfile = false -- disable swap files to avoid clutter
+vim.opt.undofile = true -- enable undo files for persistent undo
 
 -- Neovim UI
 vim.opt.relativenumber = true -- show relative line numbers
-vim.opt.cursorline = true     -- highlight the current line
-vim.opt.showmatch = true      -- show matching parenthesis
-vim.opt.ignorecase = true     -- search: ignore casing
-vim.opt.smartcase = true      -- search: obey casing containing uppercase
-vim.opt.hlsearch = true       -- search: highlight results
-vim.opt.incsearch = true      -- search: show matches as you type
-vim.opt.termguicolors = true  -- enable 24-bit RGB colors
-vim.opt.scrolloff = 2         -- number of lines above/below on scroll
-vim.opt.sidescrolloff = 2     -- number of lines left/right on side scroll
-vim.opt.wrap = true           -- enable line wrapping
-vim.opt.list = true           -- show invisible characters at end of lines
-vim.opt.listchars = {         -- customize invisible characters
+vim.opt.cursorline = true -- highlight the current line
+vim.opt.showmatch = true -- show matching parenthesis
+vim.opt.ignorecase = true -- search: ignore casing
+vim.opt.smartcase = true -- search: obey casing containing uppercase
+vim.opt.hlsearch = true -- search: highlight results
+vim.opt.incsearch = true -- search: show matches as you type
+vim.opt.termguicolors = true -- enable 24-bit RGB colors
+vim.opt.scrolloff = 2 -- number of lines above/below on scroll
+vim.opt.sidescrolloff = 2 -- number of lines left/right on side scroll
+vim.opt.wrap = true -- enable line wrapping
+vim.opt.list = true -- show invisible characters at end of lines
+vim.opt.listchars = { -- customize invisible characters
   tab = "» ",
   trail = "-",
   nbsp = "␣",
@@ -38,10 +38,11 @@ vim.opt.listchars = {         -- customize invisible characters
 }
 
 -- Tabs and Indentation
-vim.opt.expandtab = true   -- convert tabs to spaces
-vim.opt.shiftwidth = 2     -- number of spaces for each indent level
-vim.opt.tabstop = 2        -- number of spaces for a tab character
-vim.opt.autoindent = true  -- enable auto indentation
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.shiftwidth = 2 -- number of spaces for each indent level
+vim.opt.tabstop = 2 -- number of spaces for a tab character
+vim.opt.softtabstop = 2 -- number of spaces cursor to move on tab character
+vim.opt.autoindent = true -- enable auto indentation
 vim.opt.smartindent = true -- enable smart indentation
 vim.opt.breakindent = true -- enable break indentation for wrapped lines
 
