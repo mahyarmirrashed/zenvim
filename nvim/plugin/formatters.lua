4543c1c2675281a61f26c6ec84b17af7531b8b41
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-12 19:35:02 -0500

    feat(format): add basic formatters for popular languages

---
+++ nvim/plugin/formatters.lua
@@ -0,0 +1,33 @@
+require("lze").load({
+  "conform.nvim",
+  event = "BufWritePre",
+  keys = {
+    {
+      "<leader>lf",
+      function()
+        require("conform").format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        })
+      end,
+      desc = "Format Document",
+      mode = { "n", "v" },
+    },
+  },
+  after = function(_)
+    require("conform").setup({
+      formatters_by_ft = {
+        go = { "gofumpt", "goimports" },
+        json = { "jq" },
+        lua = { "stylua" },
+        nix = { "nixfmt-rfc-style" },
+        yaml = { "yamlfmt" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        timeout_ms = 500,
+      },
+    })
+  end,
+})
