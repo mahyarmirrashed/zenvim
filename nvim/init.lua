01d70ede2d61c77fbf6dc331a45cb8ced1dfd505
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-12 19:53:11 -0500

    fix(lua): actually source `lua/user/` files

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,8 @@ require("lze").register_handlers(require("lzextras").lsp)
 
 -- Ensure that cache directory is made
 vim.fn.mkdir(vim.fn.stdpath("cache"), "p")
+
+-- Source configuration modules
+require("user.autocmds")
+require("user.keymaps")
+require("user.options")
