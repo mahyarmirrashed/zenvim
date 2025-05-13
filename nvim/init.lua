37fa866e1a79c42d6967d5e335f6ac7063b58116
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-12 19:41:40 -0500

    fix(lua): ensure that cache directory is always made

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = " "
 
 -- Enables setting LSPs within the `lze` specs
 require("lze").register_handlers(require("lzextras").lsp)
+
+-- Ensure that cache directory is made
+vim.fn.mkdir(vim.fn.stdpath("cache"), "p")
