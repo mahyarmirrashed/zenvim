ca7c4b2bfc982e5f60f67ea63ee1478d5d5acd65
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-06-15 17:05:26 -0500

    refactor(mini): flatten table

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -1,12 +1,10 @@
 require("lze").load({
-  {
-    "mini.surround",
-    event = "DeferredUIEnter",
-    after = function(_)
-      require("mini.surround").setup({
-        respect_selection_type = true,
-        search_method = "cover_or_nearest",
-      })
-    end,
-  },
+  "mini.surround",
+  event = "DeferredUIEnter",
+  after = function(_)
+    require("mini.surround").setup({
+      respect_selection_type = true,
+      search_method = "cover_or_nearest",
+    })
+  end,
 })
