9e1308502e39e6bed6e7d849d139586516f5b283
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-06-25 01:09:08 -0500

    refactor(conform): remove keybinnding for saving files manually

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -1,20 +1,6 @@
 require("lze").load({
   "conform.nvim",
   event = "BufWritePre",
-  keys = {
-    {
-      "<leader>lf",
-      function()
-        require("conform").format({
-          lsp_fallback = true,
-          async = false,
-          timeout_ms = 1000,
-        })
-      end,
-      desc = "Format Document",
-      mode = { "n", "v" },
-    },
-  },
   after = function(_)
     require("conform").setup({
       formatters_by_ft = {
