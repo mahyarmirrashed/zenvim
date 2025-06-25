32c6092bf29f3dffae9690f6be14d801b9b409f4
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-06-25 01:10:39 -0500

    refactor(lazygit): do not lazy load for straight commands

--- nvim/plugin/lazygit.lua
+++ nvim/plugin/lazygit.lua
@@ -1,12 +1,5 @@
 require("lze").load({
   "lazygit.nvim",
-  cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
-  },
   keys = {
     { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
   },
