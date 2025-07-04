cc694e5ba7c337e7cd0404907c2e7e89fa5ebe36
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-07-03 21:29:59 -0500

    fix(fidget): override vim.notify

--- nvim/plugin/fidget.lua
+++ nvim/plugin/fidget.lua
@@ -1,5 +1,11 @@
 require("lze").load({
   "fidget.nvim",
   event = "DeferredUIEnter",
-  after = function(_) require("fidget").setup() end,
+  after = function(_)
+    require("fidget").setup({
+      notification = {
+        override_vim_notify = true,
+      },
+    })
+  end,
 })
