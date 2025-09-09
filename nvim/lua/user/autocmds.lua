0464bf7a9653c909413653dc0a92b1be9c1086e2
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-09-09 16:12:00 -0500

    autocmds.lua: remove spellcheck for terminal windows

--- nvim/lua/user/autocmds.lua
+++ nvim/lua/user/autocmds.lua
@@ -2,3 +2,8 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function() vim.highlight.on_yank() end,
 })
+
+-- Disable spellcheck on Terminal windows
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function() vim.opt_local.spell = false end,
+})
