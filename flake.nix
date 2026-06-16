5df6386a42c34ed87aed11214b5d69e51813f14d
Mahyar Mirrashed <mah.mirr@gmail.com>
2026-06-16 14:48:20 -0500

    *.nix: update configuration to nixpkgs 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Neovim derivation";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
     gen-luarc.url = "github:mrcjkb/nix-gen-luarc-json";
 
