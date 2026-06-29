735f0cf235cfd86efdeb89ba8654303bf7d0de83
Mahyar Mirrashed <mah.mirr@gmail.com>
2026-06-28 20:30:07 -0500

    treesitter.lua: remove lazy loading, use FileType autocmd

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,36 +1,19 @@
+require("nvim-treesitter.config").setup()
+
+require("nvim-ts-autotag").setup()
+
+vim.api.nvim_create_augroup("TreesitterHighlight", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = "TreesitterHighlight",
+  callback = function(ev) pcall(vim.treesitter.start, ev.buf) end,
+})
+
 require("lze").load({
   {
-    "nvim-treesitter",
-    dep_of = { "treesj", "nvim-ts-autotag", "auto-fix-return", "render-markdown" },
-    event = "DeferredUIEnter",
-    after = function(_)
-      require("nvim-treesitter.config").setup()
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function(ev)
-          pcall(vim.treesitter.start, ev.buf)
-        end,
-      })
-      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-        if vim.api.nvim_buf_is_loaded(buf) then
-          pcall(vim.treesitter.start, buf)
-        end
-      end
-    end,
-  },
-  {
     "treesj",
-    event = "DeferredUIEnter",
     keys = {
       { "<leader>m", function() require("treesj").toggle() end, "Toggle Splitjoin" },
     },
-    after = function(_)
-      require("treesj").setup({
-        use_default_keymaps = false,
-      })
-    end,
-  },
-  {
-    "nvim-ts-autotag",
-    after = function(_) require("nvim-ts-autotag").setup() end,
+    after = function(_) require("treesj").setup({ use_default_keymaps = false }) end,
   },
 })
